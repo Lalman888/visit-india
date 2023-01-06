65c7ce4addc00e83389b1d7866955a42763a838a
Lalman <chauhanlakshay074@gmail.com>
2023-01-06 20:26:33 +0530

    contact change

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
-        <h1 className='text-2xl font-bold text-center p-4'>Let{"'"}s work together</h1>
+        <h1 className='text-2xl font-bold text-center p-4'>
+          Any Questions? Feel free to contact us.
+        </h1>
         <form className='max-w-[600px] m-auto'>
             <div className='grid grid-cols-2 gap-2'>
                 <input className='border shadow-lg p-3' type="text" placeholder='Name' />
