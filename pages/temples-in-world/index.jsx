eff379ec3958b338d7ac9b415cf1e6a14833ae28
Lalman <chauhanlakshay074@gmail.com>
2023-02-18 14:32:26 +0530

    meta added font

--- pages/temples-in-world/index.jsx
+++ pages/temples-in-world/index.jsx
@@ -13,7 +13,7 @@ const TemplesInWorld = () => {
       {/* Overlay */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[2]' />
       <div className='relative top-[1%] p-5 text-white z-[2] mt-[-10rem]'>
-        <h2 className='text-5xl font-bold'>
+        <h2 className='text-5xl font-semibold'>
             {"Hindu Temples in World"}
         </h2>
 
