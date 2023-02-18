94c3d32117ab1bd04aac61ab147d6a272013dcb1
Lalman <chauhanlakshay074@gmail.com>
2023-02-18 14:23:39 +0530

    meta added

--- pages/temples-in-world/index.jsx
+++ pages/temples-in-world/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Head from 'next/head'
 
 const TemplesInWorld = () => {
   return (
     <>
+    <Head>
+        <title>Hindu Temples in World</title>
+        <meta name="description" content="Hindu Temples in World" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
     <div className={`flex  transition-all duration-200 ease-linear items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img-w `}>
       {/* Overlay */}
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[2]' />
       <div className='relative top-[1%] p-5 text-white z-[2] mt-[-10rem]'>
         <h2 className='text-5xl font-bold'>
             {"Hindu Temples in World"}
